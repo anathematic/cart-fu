7e5b55d7bceb21a4b56a921e4171f7ac88f450dc
Thomas <thomas@icdesign.com.au>
2010-01-19 11:09:49 +1100

    Added authlogic helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,17 +10,51 @@ class ApplicationController < ActionController::Base
   
   # I'm finding the cart so it's loaded up on the whole website, there's probably a better way to do this but I'm okay flooding someone else's cookies until then.
   before_filter :cart
+    
+   private
   
-  def cart
-    @cart = find_cart
+   def require_user
+     unless current_user
+       store_location
+       flash[:notice] = "You must be logged in to access this page"
+       redirect_to new_user_session_path
+       return false
+     end
+   end
+
+   def store_location
+     session[:return_to] = request.request_uri
+   end
+
+   def require_no_user
+     if current_user
+       store_location
+       flash[:notice] = "You must be logged out to access this page"
+       redirect_to root_url
+       return false
+     end
+   end
+   
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
   end
+
+   def current_user
+     return @current_user if defined?(@current_user)
+     @current_user = current_user_session && current_user_session.user
+   end
   
-  def find_cart 
-    session[:cart] ||= Cart.new 
-  end
-  
-  def product
-    @product = Product.find(params[:product_id])
-  end
+   def cart
+     @cart = find_cart
+   end
+
+   def find_cart 
+     session[:cart] ||= Cart.new 
+   end
+
+   def product
+     @product = Product.find(params[:product_id])
+   end
   
 end
