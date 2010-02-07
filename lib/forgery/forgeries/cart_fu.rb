d8c182ac6565aaf7a7175f1a671565e480e718f9
Thomas <thomas@icdesign.com.au>
2010-02-07 22:57:36 +1100

    Added a credit for the list of nouns

--- lib/forgery/forgeries/cart_fu.rb
+++ lib/forgery/forgeries/cart_fu.rb
@@ -1,5 +1,8 @@
 class CartFu < Forgery
 
+  # This list was thanks to
+  # http://www.momswhothink.com/reading/list-of-nouns.html
+  # Awesome!
   def self.product_name
     dictionaries[:product_names].random
   end
